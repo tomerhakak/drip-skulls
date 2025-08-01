a3ffd9393ba72622b2cf45715d6c4b373f41a8ba
Drip Skulls <dripskulls@example.com>
2025-08-02 00:56:59 +0300

    Deploy updated website to gh-pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b9ec18b8549601e1a0da6c9b017b2f4e
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "231f539129e584c03b9e3bce04f309a7",
+"flutter_bootstrap.js": "808baa4cc264584d5fa7f502c9539964",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
